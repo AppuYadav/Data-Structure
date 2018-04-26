5cccfae951c67ffefc009761b7eb61fc5db98733
Dignity <Dignity@DESKTOP-8TPHIAG>
2018-04-26 19:46:32 +0530

    Python Best Analysis

---
+++ Python/Mysql/connection.py
@@ -0,0 +1,22 @@
+'''
+Created on 24-Apr-2018
+
+@author: Dignity
+'''
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost","root","","alldatacheck" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+print('Database version : , %s' % data)
+
+# disconnect from server
+db.close()
---
+++ Python/Numpy/Basicdatatype.py
@@ -0,0 +1,20 @@
+'''
+Created on 24-Apr-2018
+
+@author: Dignity
+'''
+x = 3;
+print(type(x))
+print(x)
+print(x**3)
+
+t= True
+f = False
+
+print(type(t))
+import pandas as pd
+import seaborn as sb
+from matplotlib import pyplot as plt
+df = sb.load_dataset('iris')
+sb.distplot(df['petal_length'],kde = False)
+plt.show()
